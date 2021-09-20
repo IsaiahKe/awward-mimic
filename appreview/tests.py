0f1f5b5cc06330ad0f9c5aebefbeff518de1de41
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-20 18:51:04 +0300

    create tests

--- appreview/tests.py
+++ appreview/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import AppVote
 
 # Create your tests here.
+class Test_AppVote(TestCase):
+   
+    def setUp(self):
+        newapp=AppVote('novel','image.png','author',0.0,0.0,0.0)
+        self.assertIsInstance(newapp,AppVote)
+    def test_project(self):
+        newapp=AppVote(1,'novel','image.png','author',0.0,0.0,0.0)
+        newapp.add_project()
+        query=AppVote.objects.all()
+        self.assertTrue(len(query)==1)
+    
+        
