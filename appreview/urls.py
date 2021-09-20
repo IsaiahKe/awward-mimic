b5bd9d2e33e7d80a3a09af664ff3923e3004dded
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-20 19:05:28 +0300

    update app urls

--- appreview/urls.py
+++ appreview/urls.py
@@ -1,7 +1,19 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LoginView,LogoutView
+
 
 
 urlpatterns = [
-    url(r'^$',views.homepage,name="index")
+    url(r'^$',views.homepage,name="index"),
+    url(r'favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    url(r'^accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name="register"),
+    url(r'^accounts/logout/',LogoutView.as_view(),name="logout"),
+    url(r'^accounts/',include('django.contrib.auth.urls')),
+    url(r'^appreview/addproject/',views.addproject,name="addnewproject"),
+    url(r'^addvote/(\d)/',views.add,name="add"),
+    
 ]
