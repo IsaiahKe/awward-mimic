3663eabc6da5c0d349c183996c7daada4e71e244
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-23 22:52:58 +0300

    update model data types

--- appreview/forms.py
+++ appreview/forms.py
@@ -1,16 +1,26 @@
 from django.contrib.auth.models import User
 from django import forms
-from django.forms import widgets
-from .models import AppVote
+
+from .models import AppVote, Profile
 
 class UserForm(forms.ModelForm):
     class Meta:
         model=User
-        fields=('last_name',)
+        fields=('last_name','first_name')
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=('bio','userPhoto','contact','location')
+        
         
         
 class ProjectForm(forms.ModelForm):
     class Meta:
         model=AppVote
         exclude=['author','usability','content','design','total']
+class ProjectVote(forms.ModelForm):
+    class Meta:
+        model=AppVote
+        exclude=['author','total','livelink','total','appimage','appname']
         
--- appreview/models.py
+++ appreview/models.py
@@ -1,9 +1,11 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -11,6 +13,8 @@ class Profile(models.Model):
     userPhoto=CloudinaryField('image')
     username=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     bio=models.TextField()
+    contact=PhoneNumberField(null=True)
+    location=models.CharField(null=True,blank=True, max_length=30)
     
     @receiver(post_save, sender=User)
     def _post_save_receiver(sender,instance,created, **kwargs):
@@ -26,10 +30,11 @@ class AppVote(models.Model):
     appname=models.CharField(max_length=30,editable=True)
     appimage=CloudinaryField('image')
     author=models.CharField(max_length=30,)
-    design=models.DecimalField(max_digits=3,default=0.00,decimal_places=2)
-    usability=models.DecimalField(max_digits=3,default=0.00,decimal_places=2)
-    content=models.DecimalField(max_digits=3,default=0.00,decimal_places=2)
-    total=models.DecimalField(max_digits=4,decimal_places=2,default=0.00)
+    livelink=models.URLField(null=True, max_length=200)
+    design=models.DecimalField(max_digits=2,default=0.0,decimal_places=1,validators=[MaxValueValidator(9.9),MinValueValidator(1)])
+    usability=models.DecimalField(max_digits=3,default=0.0,decimal_places=1,validators=[MaxValueValidator(9.9),MinValueValidator(1)])
+    content=models.DecimalField(max_digits=3,default=0.0,decimal_places=1,validators=[MaxValueValidator(9.9),MinValueValidator(1)])
+    total=models.DecimalField(max_digits=6,decimal_places=4,default=0.0)
     
     
     def add_project(self):
